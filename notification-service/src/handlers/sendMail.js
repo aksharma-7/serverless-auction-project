8deaaf3debeaee800c6cd5f5e034afcc7f207202
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-06-20 14:24:07 +0530

    sent test mail

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'eu-west-1' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'atulkumar.sharma711@gmail.com',
+    Destination: {
+      ToAddresses: ['atulkumar.sharma711@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from auction project',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
